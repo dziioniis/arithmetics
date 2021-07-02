65b2b0a51106d7b0ea03c8d8e6d049882908a9d4
denisstoeglazov <dziioniis@gmail.com>
2021-07-02 12:17:42 +0300

    added operations

---
+++ src/main/java/com/github/dziioniis/Arithmetic.java
@@ -0,0 +1,16 @@
+package com.github.dziioniis;
+
+public class Arithmetic {
+    public static Integer sum(Integer numberOne,Integer numberTwo){
+        return numberOne+numberTwo;
+    }
+    public static Integer minus(Integer numberOne,Integer numberTwo){
+        return numberOne-numberTwo;
+    }
+    public static Integer multiply(Integer numberOne,Integer numberTwo){
+        return numberOne*numberTwo;
+    }
+    public static Integer divide(Integer numberOne,Integer numberTwo){
+        return numberOne/numberTwo;
+    }
+}
