c90fe5e947436258d64b1e4024cfa63ed022ecae
denisstoeglazov <dziioniis@gmail.com>
2021-07-05 10:27:14 +0300

    added something new

--- src/main/java/io/github/dziioniis/Arithmetic.java
+++ src/main/java/io/github/dziioniis/Arithmetic.java
@@ -13,4 +13,5 @@ public class Arithmetic {
     public static Integer divide(Integer numberOne,Integer numberTwo){
         return numberOne/numberTwo;
     }
+    public static void newVersionOfLibrary(){ System.out.println("new version of library");}
 }
